665416b5a50ad6aa984b166099b1e6e53ff0751a
meior <494579004@qq.com>
2017-08-20 17:20:08 +0800

    update database connection config

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     'port': process.env.PORT || 28081
-    ,'database': 'mongodb://admin:faton.Fan@127.0.0.1:27017/yipan'
+    ,'database': 'mongodb://admin:myl@127.0.0.1:27017/yipan'
     ,'secret': 'ArbeitMachtFreiSonstNichts'
     ,'uploadRemoveReplaced': true           //是否删除被覆盖和删除的文件，因为文件名是唯一的，这些文件完全可以保留
     ,'uploadDir': __dirname + '/public/uploads/'
